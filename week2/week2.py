6c67f57f73317638d1f0503e133a80e691427f82
yuyuhsiang <y43023382@gmail.com>
2024-12-25 13:12:29 +0800

    add

---
+++ week2/week2.py
@@ -0,0 +1,31 @@
+# app.py
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+
+# 用來儲存貼文的列表
+posts = []
+
+@app.route('/')
+def home():
+    return render_template('week2.html')
+
+@app.route('/api/posts', methods=['GET'])
+def get_posts():
+    return jsonify(posts)
+
+@app.route('/api/posts', methods=['POST'])
+def create_post():
+    data = request.json
+    post = {
+        'title': data['title'],
+        'content': data['content'],
+        'date': datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    }
+    posts.insert(0, post)
+    return jsonify(post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
